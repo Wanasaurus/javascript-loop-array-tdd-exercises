1d6c1456ad465f10e067f10d6b07c9290d78e841
Wanasaurus <wanasaurus@hotmail.com>
2023-10-30 15:58:48 +0800

    Repeat the string hey one time

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,6 +3,6 @@ const repeatString = function(string, num) {
 	return longString;
 	}
 
-repeatString("hey", 10);
+repeatString("hey", 1);
 // Do not edit below this line
 module.exports = repeatString;
