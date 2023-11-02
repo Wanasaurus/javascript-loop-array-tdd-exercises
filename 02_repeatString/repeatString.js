e43423da1e7ef530ef79717111e91c4fca305e69
Wanasaurus <wanasaurus@hotmail.com>
2023-11-02 16:16:00 +0800

    Enable repeat string code to return error

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,9 @@
 const repeatString = function(string, num) {
-	let longString = string.repeat(num);
-	if (num >= 0) {
-	console.log(longString);
-	} else if (num < 0) {
-	console.log("ERROR");
+	if (num < 0) {
+	return "ERROR";
 	}
+	let longString = string.repeat(num);
+	return longString;
 }
 
 repeatString("hey", -1);
