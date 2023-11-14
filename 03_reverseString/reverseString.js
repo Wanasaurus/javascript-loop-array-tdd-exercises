8625b9abd23d6ac528cdc17ef72bf02866eb3286
Wanasaurus <wanasaurus@hotmail.com>
2023-11-14 13:35:15 +0800

    Split a string into its letters in reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,5 +2,12 @@ const reverseString = function() {
 
 };
 
+//let string = "Mantine";
+let splitter = [];
+for (let i=string.length; i >= 0; i--) {
+    splitter.push(string[i]);
+}
+console.log(splitter);
+
 // Do not edit below this line
 module.exports = reverseString;
