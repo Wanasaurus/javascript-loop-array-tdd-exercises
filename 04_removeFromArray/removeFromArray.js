c314c886ae47625026455c44a035147d3141aba6
Wanasaurus <wanasaurus@hotmail.com>
2023-11-20 13:41:44 +0800

    First round of code for remove from array test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+let numbers = [[1, 2, 3, 4], 3];
+let filtered = numbers.filter(removeFromArray);
 
+function removeFromArray(numbers) {
+	return numbers !== 3;
 };
 
 // Do not edit below this line
