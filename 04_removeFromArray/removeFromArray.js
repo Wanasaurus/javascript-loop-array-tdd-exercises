b4a50fe94e4732362f83eb0944063473da245b66
Wanasaurus <wanasaurus@hotmail.com>
2023-11-22 16:43:31 +0800

    Write new code that flattens array first

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
-};
+let array = [[1, 2, 3, 4], 3];
+let flatArray = array.flat();
+let removeFromArray = flatArray.filter(function (number) {
+    return number !== 3;
+});
+console.log(removeFromArray);
 
 // Do not edit below this line
 module.exports = removeFromArray;
