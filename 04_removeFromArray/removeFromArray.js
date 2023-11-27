09a01512deaf11f6d9110fe6456fb6bc82b0dedb
Wanasaurus <wanasaurus@hotmail.com>
2023-11-27 17:42:15 +0800

    Check off remove multiple values test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -13,7 +13,7 @@ const removeFromArray = function (array, ...args) {
 };
 
 
-removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
