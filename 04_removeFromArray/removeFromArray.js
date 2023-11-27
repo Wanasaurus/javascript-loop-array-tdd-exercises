e99f9b8b91e75cc1bb12146e254ef9effb6d1a4c
Wanasaurus <wanasaurus@hotmail.com>
2023-11-27 17:53:27 +0800

    Check off ignores non present values test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -13,7 +13,7 @@ const removeFromArray = function (array, ...args) {
 };
 
 
-removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
 
 // Do not edit below this line
 module.exports = removeFromArray;
