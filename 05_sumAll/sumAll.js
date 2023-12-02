6c9915415d6299745a898eeadec0821a2e535382
Wanasaurus <wanasaurus@hotmail.com>
2023-12-02 13:21:58 +0800

    Check off add numbers between 1 and 4 test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+	let sum = 0;
+	for (let i = first; i <= second ; i++) {
+        	sum += i;
+    	}
+    	return sum;    
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
