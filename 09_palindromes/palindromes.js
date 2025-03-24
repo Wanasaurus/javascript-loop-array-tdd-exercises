fb3bd8fe5f1ecb5eff476f81d99c0d1cbec03710
Wanasaurus <wanasaurus@hotmail.com>
2025-03-24 16:29:48 +0800

    Pass works with punctuation test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,14 @@
 const palindromes = function (string) {
 
-	let ogString = string.toLowerCase().replaceAll(" ", "");
+	let ogString = string.toLowerCase().replace(/[!"#$%&'()*+,-–./———:;<=>?@[\]^_`{|}~]/g, '').replaceAll(" ", "");
 	let newString = ogString.split("").reverse().join("");
 	return newString === ogString ? true : false;
 
 };
 
 palindromes('racecar');
+
+palindromes('racecar!');
 
 // Do not edit below this line
 module.exports = palindromes;
