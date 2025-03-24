4f912800dc1658f13d13f3292f5edbf4b9b053e1
Wanasaurus <wanasaurus@hotmail.com>
2025-03-24 16:17:47 +0800

    Pass works with single words test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+	let ogString = string.toLowerCase().replaceAll(" ", "");
+	let newString = ogString.split("").reverse().join("");
+	return newString === ogString ? true : false;
 
 };
+
+palindromes('racecar');
 
 // Do not edit below this line
 module.exports = palindromes;
