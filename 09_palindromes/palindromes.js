71c63ff88e55f38ddfbf5e5059ca12bb4b685db9
Wanasaurus <wanasaurus@hotmail.com>
2025-03-25 14:12:31 +0800

    Pass works with numbers in a string test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -27,5 +27,9 @@ palindromes('Animal loots foliated detail of stool lamina.');
 
 palindromes('ZZZZ car, a man, a maracaz.');
 
+palindromes('rac3e3car');
+
+
+
 // Do not edit below this line
 module.exports = palindromes;
