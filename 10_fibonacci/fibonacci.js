1cd5f45215dc250934b4b3d9ebb2460da1da51ac
Wanasaurus <wanasaurus@hotmail.com>
2025-04-03 23:02:38 +0800

    Pass 4th Fibonacci number is 3 test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+
+	if (position === 0) return 0;
+	if (position === 1) return 1;
+	
+	let prev1 = 0;
+	let prev2 = 1;
+	let current;
+
+	for (let i = 2; i <= position; i++) {
+		current = prev1 + prev2;
+		prev1 = prev2;
+		prev2 = current;
+	}	
+	return current;
 
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
