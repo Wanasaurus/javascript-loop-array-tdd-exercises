8fda644c2f4b566c971ea91747b1be52c8e832ba
Wanasaurus <wanasaurus@hotmail.com>
2025-04-08 13:37:22 +0800

    Pass get the titles test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,19 @@
+const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
+
+
 const getTheTitles = function() {
+
+return books.map((item) => item.title);
+
 
 };
 
